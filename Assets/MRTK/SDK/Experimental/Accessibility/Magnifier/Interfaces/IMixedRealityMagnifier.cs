4d2f402500896c7b98ea820092201ea02bdb5ada
davidkline-ms <david.kline@microsoft.com>
2020-07-27 12:13:23 -0700

    add initial magnifier interface and implementation

---
+++ Assets/MRTK/SDK/Experimental/Accessibility/Magnifier/Interfaces/IMixedRealityMagnifier.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.Accessibility
+{
+    /// <summary>
+    /// Interface that defines the Mixed Reality Toolkit hologram magnifier feature.
+    /// </summary>
+    public interface IMixedRealityMagnifier : IMixedRealityService // todo: EventSystem?
+    {
+        /// <summary>
+        /// Factor by which holograms will be magnified.
+        /// </summary>
+        /// <remarks>
+        /// A factor of 1.0 corresponds to no magnification, and larger values result in a
+        /// corresponding amount of magnification. For example, a value of 1.5 makes hologram
+        /// 50 percent larger. A magnification factor less than 1.0 is not valid and will not be accepted.
+        /// </remarks>
+        // todo: what is a valid max? 5.0? 10.0?
+        float MagnificationFactor { get; set; }
+
+        /// <summary>
+        /// Collection of objects that be and remain magnified.
+        /// </summary>
+        List<GameObject> PersistentMagnification { get; }
+
+        /// <summary>
+        /// Collection of objects that should never be magnified.
+        /// </summary>
+        List<GameObject> SuppressedMagnification { get; }
+
+        /// <summary>
+        /// Suspends magnification behavior.
+        /// </summary>
+        /// <remarks>
+        /// Persistent magnification objects will return to their nomninal size.
+        /// </remarks>
+        void Suspend();
+
+        /// <summary>
+        /// Resumes magnification behavior.
+        /// </summary>
+        /// <remarks>
+        /// Persistent magnification objects will be magnified.
+        /// </remarks>
+        void Resume();
+    }
+}
---
+++ Assets/MRTK/SDK/Experimental/Accessibility/Magnifier/MixedRealityMagnifier.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.Accessibility
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MixedRealityMagnifier : BaseCoreSystem, IMixedRealityMagnifier
+    {
+        public MixedRealityMagnifier(
+            // todo: define profile
+            BaseMixedRealityProfile profile = null) : base(profile)
+        { }
+
+        /// <summary>
+        /// Reads the contents of the MixedRealityMagnifierProfile.
+        /// </summary>
+        private void ReadProfile()
+        {
+            // todo
+        }
+
+        #region IMixedRealityMagnifier
+
+        private float magnificationFactor;
+
+        /// <inheritdoc />
+        public float MagnificationFactor
+        {
+            get => magnificationFactor;
+
+            set
+            {
+                if (magnificationFactor != value)
+                {
+                    if (value < 1.0f)
+                    {
+                        Debug.LogError($"Invalid MagnificationFactor. Valid values must be greater than or equal to 1.0");
+                        return;
+                    }
+
+                    magnificationFactor = value;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public List<GameObject> PersistentMagnification => new List<GameObject>();
+
+        /// <inheritdoc />
+        public List<GameObject> SuppressedMagnification => new List<GameObject>();
+
+        /// <inheritdoc />
+        public void Suspend()
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public void Resume()
+        {
+            // todo
+        }
+
+        #endregion // IMixedRealityMagnifier
+
+        #region IMixedRealityService
+
+        /// <inheritdoc/>
+        public override string Name { get; protected set; } = "Mixed Reality Magnifier";
+
+        /// <inheritdoc />
+        public override BaseMixedRealityProfile ConfigurationProfile => null; // todo: create a profile with the magnification factor and other settings
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            ReadProfile();
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Reset()
+        {
+            //todo
+        }
+
+        /// <inheritdoc />
+        public override void Enable()
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Update()
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void LateUpdate()
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Disable()
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Destroy()
+        {
+            // todo
+        }
+
+        #endregion // IMixedRealityService
+    }
+}
