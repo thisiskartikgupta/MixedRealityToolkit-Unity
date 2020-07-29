7caf7990a7ab1569c8c3813b1c196b66af8fafe3
Joe Bieselin <joebieselin@gmail.com>
2020-07-29 09:14:48 -0700

    feat(a11y): bring closer initial service and scene
    * bring closer functionality with speech command
    * "Bring Closer" command added to speech profile keywords

---
+++ Assets/MRTK/SDK/Experimental/Accessibility/BringCloser.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Physics;
+using UnityEngine;
+
+/// <summary>
+/// Brings a hologram closer to the user.
+/// </summary>
+namespace Microsoft.MixedReality.Toolkit.Experimental.Accessibility {
+    [RequireComponent(typeof(Interpolator))]
+    public class BringCloser : MonoBehaviour
+    {
+        [Tooltip("The z-axis distance at which object will be moved to in front of Main camera.")]
+        [Range(0.3f, 5.0f)]
+        public float offset = 0.5f;
+
+        private Interpolator interpolator;
+
+        private void Awake()
+        {
+            interpolator = gameObject.GetComponent<Interpolator>();
+            
+            interpolator.SmoothLerpToTarget = true;
+        }
+
+        /// <summary>
+        /// Brings the hologram in front of the user's initial location.
+        /// </summary>
+        public void ComeTo()
+        {
+            Vector3 mainCamera = Camera.main.transform.position;
+            mainCamera.z += offset;
+            interpolator.SetTargetPosition(mainCamera);
+            transform.LookAt(Camera.main.transform.position);
+        }
+    }
+}
